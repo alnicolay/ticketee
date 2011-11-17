cdc60f4487c8dc36eebbdd943094c1e8fc3f4b48
T K <tedk81@gmail.com>
2011-11-18 04:44:55 +0800

    Added view list of projects

--- features/step_definitions/creating_projects.rb
+++ features/step_definitions/creating_projects.rb
@@ -2,6 +2,10 @@ Given /^I am on the homepage$/ do
   visit "/"
 end
 
+Given /^there is a project called "([^\"]*)"$/ do |name|
+  Factory(:project, :name => name)
+end
+
 When /^I follow "([^"]*)"$/ do |arg1|
   click_link(arg1)
 end
